83ec5328d656802ffc803978d950af70e435c65a
mist258 <natalia.kolchuk@gmail.com>
2025-06-30 23:16:11 +0300

    added settings gor media files

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
 
+
     # my apps
     'core',
     'apps.initial',
@@ -121,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'storage')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
