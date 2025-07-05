d7a536de3494aeaf5634823f0405575440ece41d
mist258 <natalia.kolchuk@gmail.com>
2025-07-06 01:58:40 +0300

    deleted frontend & client, comment web container & nginx

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+
 CORS_ALLOWED_ORIGINS = [
      "http://localhost:3000",
 ]
@@ -43,8 +44,6 @@ INSTALLED_APPS = [
     'django_filters',
     'corsheaders',
     'drf_yasg',
-
-
 
     # my apps
     'core',
