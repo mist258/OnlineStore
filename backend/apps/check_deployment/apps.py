115b8312675a347803f039e5a83622fbc7114dd9
mist258 <natalia.kolchuk@gmail.com>
2025-07-07 19:26:10 +0300

    fix

--- backend/apps/check_deployment/apps.py
+++ backend/apps/check_deployment/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CheckDeploymentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'check_deployment'
+    name = 'apps.check_deployment'
