2ec17d4355cf07d148de8aa89f1da08eeb2fe5ca
mist258 <natalia.kolchuk@gmail.com>
2025-07-08 22:58:22 +0300

    delete initial app

--- backend/apps/initial/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import InitialAddPhotoView, InitialTestView
-
-urlpatterns = [
-    path('', InitialTestView.as_view(), name='initial_test'),
-    path('/<int:pk>/photo', InitialAddPhotoView.as_view(), name='initial_add_photo'),
-
-]
