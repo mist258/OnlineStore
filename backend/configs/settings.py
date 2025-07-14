7ac4b6226245891deb0f5594dcefbdc16c7265b3
mist258 <natalia.kolchuk@gmail.com>
2025-07-14 22:35:42 +0300

    setup configs for photo loading

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -144,9 +144,9 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'storage')
-MEDIA_URL = "/media/"
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'storage')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- backend/configs/urls.py
+++ backend/configs/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls.static import static
 from django.urls import include, path
 
 from rest_framework.permissions import AllowAny
 
+from django_filters.conf import settings
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
@@ -43,3 +45,5 @@ urlpatterns = [
         name="schema-swagger",
     ),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
