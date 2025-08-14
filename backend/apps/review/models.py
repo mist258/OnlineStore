829a45abe48ea6adba9a5d760ceaf720b1efe510
mist258 <natalia.kolchuk@gmail.com>
2025-08-14 15:49:53 +0300

    delete FK in 'review'

--- backend/apps/review/models.py
+++ backend/apps/review/models.py
@@ -20,11 +20,6 @@ class Review(models.Model):
         on_delete=models.CASCADE,
         related_name='reviews'
     )
-    product = models.ForeignKey(
-        Product,
-        on_delete=models.CASCADE,
-        related_name='reviews'
-    )
 
     def __str__(self):
         return f"Review {self.grade} ★ by {self.customer.email}"
