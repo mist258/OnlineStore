077c52c576fe8f3bc61995b7053f333c4b5dfeeb
mist258 <natalia.kolchuk@gmail.com>
2025-10-15 12:55:51 +0300

    fix search

--- backend/apps/search/views.py
+++ backend/apps/search/views.py
@@ -1,13 +1,20 @@
+from django.utils.decorators import method_decorator
+
 from rest_framework.permissions import AllowAny
 
 from apps.accessories.models import Accessory
 from apps.products.models import Product
 
 from drf_haystack.viewsets import HaystackViewSet
+from drf_yasg.utils import swagger_auto_schema
 
 from .serializers import GlobalSearchSerializer
 
 
+@method_decorator(name='get', decorator=swagger_auto_schema(
+    security=[],
+    operation_id='search_products_and_accessories'
+))
 class GlobalItemSearchView(HaystackViewSet):
     """
         Global search view
