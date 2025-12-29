6c25a0b6e390c636c5e826cc709b292cc212b200
mist258 <natalia.kolchuk@gmail.com>
2025-12-29 23:37:26 +0200

    added template to create subscription

--- backend/apps/subscription/urls.py
+++ backend/apps/subscription/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from .views import SubscribeUserForUpdateView, UnsubscribeUserFromUpdateView
+
 urlpatterns = [
+    path('/subscribe', SubscribeUserForUpdateView.as_view()),
+    path('/unsubscribe', UnsubscribeUserFromUpdateView.as_view()),
     
 ]
--- backend/apps/subscription/views.py
+++ backend/apps/subscription/views.py
@@ -1,3 +1,13 @@
 from django.contrib.auth import get_user_model
 
+from rest_framework import generics
+
 UserModel = get_user_model()
+
+
+class SubscribeUserForUpdateView(generics.GenericAPIView):
+    pass
+
+
+class UnsubscribeUserFromUpdateView(generics.GenericAPIView):
+    pass
