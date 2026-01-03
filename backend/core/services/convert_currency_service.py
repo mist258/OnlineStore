7f70e5b2baefb8998accbbf7051c321071615263
mist258 <natalia.kolchuk@gmail.com>
2026-01-03 22:58:18 +0200

    fix

--- backend/core/services/convert_currency_service.py
+++ backend/core/services/convert_currency_service.py
@@ -9,17 +9,11 @@ class CurrencyService:
     def get_exchange_rates():
         res = requests.get('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11')
 
-        if res.status_code == 200:
-            data = res.json()
-            rates = {}
+        try:
+            if res.status_code == 200:
+                data = res.json()
+                return data
 
-            for rate in data:
+        except Exception as e:
+            return e
 
-                if rate.get('ccy') in ['USD', 'EUR']:
-                    rates[rate['ccy']] = {
-                        'sale': Decimal(str(rate['sale'])),
-                        'purchase': Decimal(str(rate['buy'])),
-                    }
-            return rates
-
-        return None
