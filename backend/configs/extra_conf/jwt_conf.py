bea90e67a537c5b118f543c00f7ebc03198beb6f
mist258 <natalia.kolchuk@gmail.com>
2026-01-06 14:21:22 +0200

    fix jwt

--- backend/configs/extra_conf/jwt_conf.py
+++ backend/configs/extra_conf/jwt_conf.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=5),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": True,
